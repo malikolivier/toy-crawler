00000010929be9c0337fa75d8ae8ae02b5afd9a4
Malik Olivier Boussejra <malik@boussejra.com>
2022-12-01 17:54:15 +0900

    Section 1
    - fetch URLs and save the content to files in the file system
    
    Can run the program with:
    
    	cargo run -- https://www.google.com https://blog.epigno.jp
    
    If any error occurs, a string representation of the raised error will be
    printed on the console (not really nice of human consuptions, but,
    writing clean error messages would take more time).

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::fs;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version)]
+struct Args {
+    #[arg(
+        value_name = "URL",
+        required = true,
+        help = "URLs to retrieve, e.g. 'https://google.com'"
+    )]
+    urls: Vec<String>,
+}
+
+fn fetch(url: &str) -> Result<(), Box<dyn Error>> {
+    let bytes = reqwest::blocking::get(url)?.bytes()?;
+    let url_last_name = url
+        .split('/')
+        .filter(|s| !s.is_empty())
+        .last()
+        .ok_or("URL cannot be broken down into a filename")?;
+    let filename = format!("{}.html", url_last_name);
+    fs::write(&filename, bytes)?;
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+    for url in args.urls {
+        fetch(&url)?;
+    }
+
+    Ok(())
 }
